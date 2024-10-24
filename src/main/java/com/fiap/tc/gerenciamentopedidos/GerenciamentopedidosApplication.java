ea83db286e2dd5b6ff663bae45e488eeb93bf260
MarcosHRFerreira <marcoshrferreira@gmail.com>
2024-10-24 10:09:21 -0300

    carga inicial

---
+++ src/main/java/com/fiap/tc/gerenciamentopedidos/GerenciamentopedidosApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.tc.gerenciamentopedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentopedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentopedidosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/tc/gerenciamentopedidos/GerenciamentopedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.tc.gerenciamentopedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentopedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
